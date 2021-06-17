e3313a4c822cc95267094155e718c54d1cda33d0
LenaChestnut <lenachestnut@gmail.com>
2021-06-17 17:02:10 +0300

    Add scoreboard

---
+++ src/components/Scoreboard/Scoreboard.js
@@ -0,0 +1,12 @@
+import './Scoreboard.scss';
+
+function Scoreboard() {
+  return (
+    <div className="scoreboard">
+      <p className="scoreboard__score">Current score: 0</p>
+      <p className="scoreboard__score">Best score: 0</p>
+    </div>
+  );
+}
+
+export default Scoreboard;
