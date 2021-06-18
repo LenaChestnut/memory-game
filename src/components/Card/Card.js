5fdef9b5f3a8eed0c0456ad81ab82209104cdf8e
LenaChestnut <lenachestnut@gmail.com>
2021-06-18 18:36:42 +0300

    Style cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,10 @@ import './Card.scss';
 function Card() {
   return (
     <div className="card">
-      <p>Card</p>
+      <div className="card__image-container">
+        <img className="card__image" alt="text" />
+      </div>
+      <p className="card__title">Name</p>
     </div>
   );
 }
