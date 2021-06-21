983c8f1ff8a30e71ae566b21c07d84b92c7b2b48
LenaChestnut <lenachestnut@gmail.com>
2021-06-21 19:03:33 +0300

    Add image attributes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,20 @@
 import './Card.scss';
+import data from '../../data/data.json';
 
 function Card() {
   return (
     <div className="card">
       <div className="card__image-container">
-        <img className="card__image" alt="text" />
+        <img
+          className="card__image"
+          alt="text"
+          width="110px"
+          height="110px"
+          // src="img"
+          // srcset="img 2x"
+        />
       </div>
-      <p className="card__title">Name</p>
+      <p className="card__title">{data[0].name}</p>
     </div>
   );
 }
